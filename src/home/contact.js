33510bc971f819dfdc99b7c78086d02ad248569c
Iman Maliki <45257900+imanmaliki11@users.noreply.github.com>
2020-07-27 10:30:10 +0700

    adding content in contact.js

--- src/home/contact.js
+++ src/home/contact.js
@@ -6,22 +6,19 @@ import icon from './../img/iman.jpg';
 
 function Contact() {
     return(
-        <div className="container px-5 py-2">
-            <div className="contact-card rounded px-5 py-2">
-
-            <h1 className="mb-3">My Contact</h1>
-
+        <div className="container px-5">
+            <h1 className="mb-2 font-nav">My Contact</h1>
                 <Row>
-                    <Col sm>
+                    <Col sm className="px-3">
                         <Row className="justify-content-center">
-                            <h3 className="font-name">IMAN MALIKI</h3>
+                            <h5 className="font-name">IMAN MALIKI</h5>
                         </Row>
                         <Row className="justify-content-center">
                             <Col sm={8} className="p-3">
                                 <img className="img-square rounded" src={icon} alt="Iman Maliki" />
                             </Col>
                         </Row>
-                        <Row className="text-justify">
+                        <Row className="text-justify px-2">
                             <p className="font-body">
                                 Definisi Merupakan suatu batasan atau arti, bisa juga dimaknai kata,
                                 frasa, atau kalimat yang mengungkapkan makna, keterangan, atau ciri
@@ -35,12 +32,44 @@ function Contact() {
                             </p>
                         </Row>
                     </Col>
-                    <Col sm>
-                        Find Me On
+
+                    <Col sm className="px-3">
+                        <Row className="justify-content-center mb-2">
+                            <h4 className="font-name">Contact</h4>
+                        </Row>
+                        <Row className="justify-content-start px-2 mb-4">
+                            <h4 className="font-name">
+                                <img alt="googel" className="mr-2" src="https://img.icons8.com/cute-clipart/64/000000/google-logo.png"/>
+                                imanmaliki11[at]gmail[dot]com
+                            </h4>
+                        </Row>
+                        <Row className="justify-content-start px-2 mb-4">
+                            <h4 className="font-name"> 
+                                <img alt="ig" className="mr-2" src="https://img.icons8.com/cute-clipart/64/000000/phone.png"/>
+                                +62838 6099 3225
+                            </h4>
+                        </Row>
+                        <Row className="justify-content-start px-2 mb-4">
+                            <h4 className="font-name text-justify"> 
+                            <img alt="linkedin" className="mr-2" src="https://img.icons8.com/cute-clipart/64/000000/linkedin.png"/>
+                                imanmaliki11
+                            </h4>
+                        </Row>
+                        <Row className="justify-content-start px-2 mb-4">
+                            <h4 className="font-name text-justify"> 
+                                <img alt="ig" className="mr-2" src="https://img.icons8.com/cute-clipart/64/000000/github.png"/>
+                                GitHub.com/imanmaliki11
+                            </h4>
+                        </Row>
+                        <Row className="justify-content-start px-2 mb-4">
+                            <h4 className="font-name"> 
+                                <img alt="ig" className="mr-2" src="https://img.icons8.com/cute-clipart/64/000000/instagram-new.png"/>
+                                imanmalikii
+                            </h4>
+                        </Row>
                     </Col>
                 </Row>
             </div>
-        </div>
     );
 }
 
